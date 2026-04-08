7e72d926570b2b0854e3919ccab782e4f8c13aa9
rahul yadav <yadavrahul784ej@gmail.com>
2026-04-08 20:40:49 +0530

    third commit

--- rendering-demo/src/app/dashboard/page.jsx
+++ rendering-demo/src/app/dashboard/page.jsx
@@ -1,13 +1,21 @@
-"use client";
-import { useState } from "react";
+// "use client";
+// import { useState } from "react";
+
+// export default function Dashboard() {
+//     const [name,setName] = useState("");
+//     console.log(name);
+//     return (
+//         <div>
+//             <h1>Dashboard</h1>
+//             <input type="text" placeholder="input here" value={name} onChange={(e) => setName(e.target.value)} />
+//             <p>{name}</p>
+//         </div>
+//     )}
 
 export default function Dashboard() {
-    const [name,setName] = useState("");
-    console.log(name);
     return (
         <div>
-            <h1>Dashboard</h1>
-            <input type="text" placeholder="input here" value={name} onChange={(e) => setName(e.target.value)} />
-            <p>{name}</p>
+            <p> dashboard: {new Date().toLocaleTimeString()}</p>
+
         </div>
     )}
